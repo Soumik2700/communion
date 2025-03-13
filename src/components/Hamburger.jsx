a4fa1bac3c57b33919bcabf025e172e8f239a0ac
Soumik Sinha <soumiksingha8@gmailcom>
2025-03-13 20:43:13 +0530

    links are working for the small screens also

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,10 +1,12 @@
-function Hamburger(){
-    return(
+import { Link } from "react-router-dom";
+
+function Hamburger() {
+    return (
         <section className="h-300px p-5 bg-gray-100 absolute min-w-screen">
             <ul className="font-bold text-gray-500">
-                <li>Home</li>
-                <li>Events</li>
-                <li>About</li>
+                <Link to={"/"}><li>Home</li></Link>
+                <Link to={"/events"}><li>Events</li></Link>
+                <Link to={"/about"}><li>About</li></Link>
             </ul>
 
             <button className="mt-3 w-full bg-gray-700 text-white font-bold rounded-3xl hover:bg-blue-600 transition-all">
